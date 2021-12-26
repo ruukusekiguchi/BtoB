a26739ec675fc2d1c7ccba0680b26039bd5052c9
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2021-12-26 17:08:56 +0900

    リポジトリ作成

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// var price2 = $("#price2").html();　//string
+// price2 = price2 * 10; //number
+// price2 = String(price2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,'); //string
+// $("#result2").html(price2);
+
