92d32dda7cb2bac60d89535078e2db8558f1d254
UchidaKota <gilchan0822@gmail.com>
2022-01-11 16:52:19 +0900

    uchidaver6

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream:js/index.js
 fetch("header.html")
+=======
+fetch("../partials/header.ejs")
+>>>>>>> Stashed changes:views/js/index.js
 .then(response => {
     return response.text()
 })
