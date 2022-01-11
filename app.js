097246af91b951079e41b03402cdcba01c50b1b9
UchidaKota <gilchan0822@gmail.com>
2022-01-11 15:55:57 +0900

    uchidaver3

--- app.js
+++ app.js
@@ -1,78 +1,31 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-const {engine} = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
-const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo');
-const methodOverride = require('method-override');
-//Import Routes
-const authRoute = require('./routes/auth');
-const postRoute = require('./routes/posts');
-
-dotenv.config();
-require('./config/passport')(passport);
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true},() => 
-    console.log("connect to db")
-);
 
 //Body parser
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-//Method Override
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      var method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-}));
-
-//Handlebars Helpers
-const {formatDate, truncate, stripTags, editIcon, select} = require('./helpers/hbs');
-
 //Handlebars
-app.engine('.hbs', engine({
-    helpers: {
-        formatDate,
-        truncate,
-        stripTags,
-        editIcon,
-        select
-    }, 
-    defaulyLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
 
 //Sessions
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({mongoUrl: process.env.DB_CONNECT})
+    saveUninitialized: false
 }));
 
 //Passport Middlewares
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Set global var
-app.use(function (req, res, next) {
-    res.locals.user = req.user || null
-    next();
-});
-
 //Static folder
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public', {index: false}));
+app.use(express.static(__dirname + '/views', {index: false}));
 
 //Routes
 app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
-app.use('/stories', require('./routes/stories'));
 
 app.listen(3000, () => console.log("server run and up"));
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
 const router = require('express').Router();
-const {ensureAuth, ensureGuest} = require('../middleware/auth');
 
-const Story = require('../model/Story');
-
-router.get('/', ensureGuest, (req, res) => {
-    res.render('login.hbs', {
-        layout: 'login'
-    });
-});
-
-router.get('/dashboard', ensureAuth, async (req, res) => {
-    try{
-        const stories = await Story.find({user: req.user.id}).lean();
-        res.render('dashboard.hbs', {
-            name: req.user.firstName,
-            stories
-        });
-    } catch(err){
-        console.log(err);
-        res.render('error/500.hbs');
-    }
+router.get('/', (req, res) => {
+    res.render('login.ejs');
 });
 
 module.exports = router;
