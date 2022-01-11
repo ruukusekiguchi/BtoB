32d1458e911ac9f39e4f7a827b933a817263f68f
UchidaKota <gilchan0822@gmail.com>
2022-01-11 16:52:40 +0900

    aa

---
+++ views/js/index.js
@@ -0,0 +1,17 @@
+fetch("../partials/header.ejs")
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    document.querySelector("header").innerHTML = data;
+});
+
+console.log("出力");
+
+// fetch("footer.html")
+// .then(response => {
+//     return response.text()
+// })
+// .then(data => {
+//     document.querySelector("footer").innerHTML = data;
+// });
