7395e8c317a54d62cf024895f7d999af584f7c13
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-01-13 11:23:23 +0900

    a

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,22 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-<<<<<<< HEAD
-//ログイン認証
-=======
-//ログイン後表示：オークション画面
-//route GET, '/auction', index.ejs
->>>>>>> origin/uchidabranch
 router.post('/login', (req, res) => {
     console.log(req.body);
     res.redirect('/auction');
 });
 
-<<<<<<< HEAD
-//ログアウト処理
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-=======
+
+// //ログアウト処理
+// router.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+
 //ログアウト
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
->>>>>>> origin/uchidabranch
+
 });
 
 module.exports = router;
