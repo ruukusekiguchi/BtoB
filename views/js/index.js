6a72c37b2bf28720646a009f6f6ac33241f1a62c
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-01-18 23:29:07 +0900

    aaa

--- views/js/index.js
+++ views/js/index.js
@@ -5,13 +5,4 @@ fetch("../partials/header.ejs")
 .then(data => {
     document.querySelector("header").innerHTML = data;
 });
-
 console.log("出力");
-
-// fetch("footer.html")
-// .then(response => {
-//     return response.text()
-// })
-// .then(data => {
-//     document.querySelector("footer").innerHTML = data;
-// });
