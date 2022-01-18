de7b79c4b2c70fb6394f3850e4811449d812be8b
UchidaKota <gilchan0822@gmail.com>
2022-01-18 19:21:27 +0900

    auction

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,4 @@ router.get('/', (req, res) => {
     res.render('login.ejs');
 });
 
-//ログイン後表示：オークション画面
-//route GET, '/auction', index.ejs
-router.get('/auction', (req, res) => {
-    res.render('index.ejs');
-});
-
-//会員登録：会員登録画面
-//route GET, '/register', register.ejs
-
-router.get('/register', (req, res) => {
-    res.render('register.ejs');
-});
-
-//詳細ボタン後表示：オークション詳細画面
-//route GET, '/auction/id', detail.ejs
-router.get('/auction/id', (req, res) => {
-    res.render('detail.ejs');
-});
-
-//会員登録処理
-//route POST, '/register/edit'
-router.post('/register/edit', (req, res) => {
-    console.log(req.body);
-});
-
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: '3306',
+    database: 'auction_master'
+});
+
+//会員登録：会員登録画面
+//route GET, '/user/add',
+router.get('/add', (req, res) => {
+    res.render('register.ejs');
+    console.log('とんでるよ');
+});
+
+//会員登録処理
+//route POST, '/user'
+router.post('/', (req, res) => {
+    const address = req.body.prefecture + req.body.city + req.body.block + req.body.building;
+    values = [
+        req.body.user_id,
+        req.body.name,
+        address,
+        req.body.mail,
+        req.body.password,
+    ];
+    console.log(values);
+    connection.query("INSERT INTO user_info(user_id,user_name,user_address,user_email,user_pass) VALUES(?,?,?,?,?);", values, (error, results) => {
+        if(error){
+            res.status(400).send({values:'Error'});
+            return;
+        }
+        console.log('ユーザー登録完了');
+        console.log(results);
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
