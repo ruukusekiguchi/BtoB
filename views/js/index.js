a935278fe0163173c8f588c4777d7304d94c69e3
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-01-21 02:36:11 +0900

    いらんフォルダを削除

--- views/js/index.js
+++
@@ -1,8 +0,0 @@
-fetch("../partials/header.ejs")
-.then(response => {
-    return response.text()
-})
-.then(data => {
-    document.querySelector("header").innerHTML = data;
-});
-console.log("出力");
