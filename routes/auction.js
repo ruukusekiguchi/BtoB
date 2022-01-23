d8a14d1063bca59b38e9e4fe620275fde5b757b5
UchidaKota <gilchan0822@gmail.com>
2022-01-23 18:05:31 +0900

    初めてのオークション

--- routes/auction.js
+++ routes/auction.js
@@ -30,14 +30,39 @@ router.get('/:userid/:auctionid', (req, res) => {
     let values = [
         req.params.auctionid
     ];
-    connection.query("SELECT a.*, cd.*, COUNT(bh.auction_id) AS bid_num FROM auction a INNER JOIN car_detail cd ON a.car_detail_id = cd.id INNER JOIN bid_history bh ON a.id = bh.auction_id WHERE a.id=? GROUP BY a.id;", values, (error, results) => {
-        if(error){
-            console.log('error connecting:' + error.stack);
-            return;
-        }
-        console.log("車情報取得");
+    connection.query("SELECT auction_id FROM bid_history WHERE auction_id=? GROUP BY auction_id;", values, (error, results) => {
         console.log(results);
-        res.render("./detail.ejs", {data:results, userid:req.params.userid, auctionid:req.params.auctionid});
+        if(results == ""){
+            console.log('空だよ');
+            let values = [
+                req.params.auctionid
+            ];
+            connection.query("SELECT * FROM auction a INNER JOIN car_detail cd ON a.car_detail_id = cd.id WHERE a.id=?;", values, (error, results) => {
+                if(error){
+                    console.log('error connecting:' + error.stack);
+                    return;
+                }
+                console.log("車情報取得");
+                console.log(results);
+                results[0].bid_num = 0;
+                console.log(results);
+                res.render("./detail.ejs", {data:results, userid:req.params.userid, auctionid:req.params.auctionid});
+            });
+        }
+        else{
+            let values = [
+                req.params.auctionid
+            ];
+            connection.query("SELECT a.*, cd.*, COUNT(bh.auction_id) AS bid_num FROM auction a INNER JOIN car_detail cd ON a.car_detail_id = cd.id INNER JOIN bid_history bh ON a.id = bh.auction_id WHERE a.id=? GROUP BY a.id;", values, (error, results) => {
+                if(error){
+                    console.log('error connecting:' + error.stack);
+                    return;
+                }
+                console.log("車情報取得");
+                console.log(results);
+                res.render("./detail.ejs", {data:results, userid:req.params.userid, auctionid:req.params.auctionid});
+            });
+        }
     });
 });
 
