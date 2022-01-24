a8916580bcead24d66f655472a0144ddac3e5a82
shimomura551 <shimomura551@icloud.com>
2022-01-25 02:19:05 +0900

    sql

--- car_detail.sql
+++ car_detail.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2022-01-24 18:12:27
+-- 生成日時: 2022-01-24 18:17:55
 -- サーバのバージョン： 10.4.17-MariaDB
 -- PHP のバージョン: 8.0.1
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 -- テーブルの構造 `car_detail`
 --
 
+DROP TABLE IF EXISTS `car_detail`;
 CREATE TABLE IF NOT EXISTS `car_detail` (
   `id` varchar(17) NOT NULL,
   `car_maker` varchar(16) NOT NULL,
@@ -67,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `car_detail` (
 
 INSERT INTO `car_detail` (`id`, `car_maker`, `car_name`, `car_age`, `car_model`, `car_shape`, `car_grade`, `car_evapoint`, `car_changesys`, `car_levelposition`, `car_gear`, `car_ecolor`, `car_ecolorno`, `car_icolor`, `car_history`, `car_mileage`, `car_door`, `car_crew`, `car_drivesys`, `car_fuel`, `car_displacement`, `car_inspection`, `car_certification`, `car_instruction`, `car_repairhistoroy`, `car_foreign`, `car_fstatus`, `car_fhandle`, `car_spare`, `car_remarks`) VALUES
 ('3BDS321V-239413', 'ダイハツ', 'ハイゼットカーゴ', 7, '3BD-S321V', 'W', 'CTDバージョン', 4, 0, 1, 4, 'ホワイト', 'KR4', 'シルバー', 5, 12000, 5, 4, '2WD', 'ガソリン', 1500, 0, 1, 1, 0, 0, NULL, NULL, 1, '保証書'),
-('5BALA250S-100123', 'ダイハツ', 'キャスト スタイル', 4, '5BA-LA250S', 'SW', 'CTDバージョン', 4, 0, 1, 4, 'ブラック', '199', 'グレー', 2, 4000, 5, 4, '2WD', 'ガソリン', 2500, 0, 1, 1, 0, 0, NULL, NULL, 1, 'ワンオーナー'),
+('5BALA250S-100123', 'ダイハツ', 'タフト', 4, '5BA-LA250S', 'SW', 'CTDバージョン', 4, 0, 1, 4, 'グリーン', '199', 'グレー', 2, 4000, 5, 4, '2WD', 'ガソリン', 2500, 0, 1, 1, 0, 0, NULL, NULL, 1, 'ワンオーナー'),
 ('5BALA250S-189402', 'ダイハツ', 'キャストスタイル', 6, '5BA-LA250S', 'W', 'CTDバージョン', 4, 0, 1, 4, 'ブラック', '199', 'グレー', 4, 10000, 5, 4, '2WD', 'ガソリン', 1000, 0, 1, 1, 0, 0, NULL, NULL, 1, 'ワンオーナー'),
 ('5BALA350S-109203', 'ダイハツ', 'ミラ イース', 8, '5BA-LA350S', 'SW', 'CTDバージョン', 4, 0, 1, 4, 'ホワイト', 'KR4', 'グレー', 6, 20500, 5, 4, '2WD', 'ガソリン', 1000, 0, 1, 1, 0, 0, NULL, NULL, 1, 'LEDヘッドライト　フロアマット純正'),
 ('DAAMH55S-123550', 'スズキ', 'ワゴンR', 3, 'DAA-MH55S', 'W', 'CTDバージョン', 4, 0, 1, 4, 'ホワイト', 'KR4', 'グレー', 3, 10000, 5, 4, '4WD', 'ガソリン', 1000, 0, 1, 1, 0, 0, NULL, NULL, 1, 'リサイクル料：リ済別'),
